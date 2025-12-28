b302726195d95c072ca9242fe8e40f8cef3aca28
Mr.ShivdasSarkar <sarkarindustries77@gmail.com>
2025-12-28 22:39:52 +0530

    Added local fonts
    Removed online dependency on fonts

--- app/_layout.js
+++ app/_layout.js
@@ -1,42 +1,45 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Slot } from "expo-router";
 import { Platform, Text, View } from "react-native";
 import * as NavigationBar from "expo-navigation-bar";
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { useFonts } from "expo-font";
-import {
-    Poppins_400Regular,
-    Poppins_500Medium,
-    Poppins_600SemiBold,
-    Poppins_700Bold,
-} from "@expo-google-fonts/poppins";
+import * as Font from "expo-font";
 
 export default function Layout() {
-    useEffect(() => {
-        if (Platform.OS === "android") {
-            NavigationBar.setVisibilityAsync("hidden").catch(console.warn);
-        }
-    }, []);
-    const [fontsLoaded] = useFonts({
-        Poppins_400Regular,
-        Poppins_500Medium,
-        Poppins_600SemiBold,
-        Poppins_700Bold,
-    });
+  const [fontsLoaded, setFontsLoaded] = useState(false);
 
-    if (!fontsLoaded) {
-        return <View style={{ flex: 1, backgroundColor: "#000" }} />;
+  useEffect(() => {
+    if (Platform.OS === "android") {
+      NavigationBar.setVisibilityAsync("hidden").catch(console.warn);
     }
-    if (!Text.defaultProps) Text.defaultProps = {};
-    Text.defaultProps.style = {
-        ...(Text.defaultProps.style || {}),
-        fontFamily: "Poppins_400Regular",
-    };
-    return (
-        <View style={{ flex: 1, backgroundColor: "#111" }}>
-            <SafeAreaView style={{ flex: 1 }}>
-                <Slot />
-            </SafeAreaView>
-        </View>
-    );
+
+    async function loadFonts() {
+      await Font.loadAsync({
+        Poppins_400Regular: require("../assets/fonts/Poppins-Regular.ttf"),
+        Poppins_500Medium: require("../assets/fonts/Poppins-Medium.ttf"),
+        Poppins_600SemiBold: require("../assets/fonts/Poppins-SemiBold.ttf"),
+        Poppins_700Bold: require("../assets/fonts/Poppins-Bold.ttf"),
+      });
+      setFontsLoaded(true);
+    }
+    loadFonts();
+  }, []);
+
+  if (!fontsLoaded) {
+    return <View style={{ flex: 1, backgroundColor: "#000" }} />;
+  }
+
+  if (!Text.defaultProps) Text.defaultProps = {};
+  Text.defaultProps.style = {
+    ...(Text.defaultProps.style || {}),
+    fontFamily: "Poppins_400Regular",
+  };
+
+  return (
+    <View style={{ flex: 1, backgroundColor: "#111" }}>
+      <SafeAreaView style={{ flex: 1 }}>
+        <Slot />
+      </SafeAreaView>
+    </View>
+  );
 }
